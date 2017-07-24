ca21f9cc79a105dfd82b39538636d4bc0062fdfe
HaiRui <zhengjianwen@users.noreply.github.com>
2017-07-24 16:16:48 +0800

    hairui
    new code

---
+++ tourist_guide/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TouristGuideConfig(AppConfig):
+    name = 'tourist_guide'
---
+++ viewpoint/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ViewpointConfig(AppConfig):
+    name = 'viewpoint'
